a3e1ac0dbcb017ac99352a4f428482f442f51f25
Hachem Zarrad <hachemzarrad@gmail.com>
2021-03-27 01:39:13 +0100

    Before Expo Update

--- screens/authentication/signUpScreen.js
+++ screens/authentication/signUpScreen.js
@@ -1,4 +1,4 @@
-import React, { useReducer, useEffect, useCallback, useState } from 'react'
+import React, { useReducer, useEffect, useCallback } from 'react'
 import { StyleSheet, View, ScrollView, KeyboardAvoidingView } from 'react-native'
 
 import InputBar from '../../components/inputBar'
