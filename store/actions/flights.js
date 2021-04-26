e4a8762190b194efc014dccd9d9f9e231ad57991
Hachem Zarrad <hachemzarrad@gmail.com>
2021-04-26 14:04:37 +0100

    Refactoring Flights Actions And Reducers

--- store/actions/flights.js
+++ store/actions/flights.js
@@ -3,15 +3,13 @@ import { baseUrl } from '../../constants/networking';
 
 
 export const fetchFlights = () => async (dispatch) => {
-    dispatch({ type: ActionTypes.FLIGHTS_LOADING })
     try {
         const response = await fetch(`${baseUrl}flights`)
-        if (!response.ok) throw Error('Please Check Your Internet Connection')
+        if (!response.ok) throw new Error('Please Check Your Internet Connection')
         const  flights = await response.json()
         dispatch({ type: ActionTypes.GET_FLIGHTS, payload: flights })
     }
     catch (error) {
-        dispatch({ type: ActionTypes.FLIGHTS_FAILED, payload: error })
         throw error
     }
 }
--- store/reducers/flights.js
+++ store/reducers/flights.js
@@ -2,18 +2,12 @@ import * as ActionTypes from '../actions/actionTypes';
 
 const initialState = {
     flights: [],
-    error: null,
-    loading: true
 }
 
 export default (state = initialState, action) =>{
     switch(action.type) {
         case(ActionTypes.GET_FLIGHTS):
-            return {...state, flights: action.payload, error: null, loading: false};
-        case(ActionTypes.FLIGHTS_LOADING):
-            return {...state, flights: [], error: null, loading: true};
-        case(ActionTypes.FLIGHTS_FAILED):
-            return{...state, flights: [], error: action.payload, loading: false};
+            return {...state, flights: action.payload};
 
         default: 
             return state;
