f9b3959c41e5350d89acfb68dea40d4a5ece1b9f
Hachem Zarrad <hachemzarrad@gmail.com>
2021-04-27 13:26:16 +0100

    Reactoring Splash Screen Part3

--- screens/splashScreen.js
+++ screens/splashScreen.js
@@ -1,10 +1,10 @@
-import React, { useCallback, useEffect, useReducer } from 'react'
+import React, { useEffect, useReducer } from 'react'
 import {
-  StyleSheet, Text, View, Image, ActivityIndicator,
+  StyleSheet, Text, View, Image, ActivityIndicator, Alert
 } from 'react-native'
 
 import { useNavigation } from '@react-navigation/native'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
 import Colors from '../constants/colors'
 
@@ -51,41 +51,41 @@ const SplashScreen = () => {
     countries_CodesError: null,
   })
 
-
   const dataLoading = dataState.hotelsLoading && dataState.destinationsLoading && dataState.flightsLoading && dataState.trainsLoading && dataState.busesLoading && dataState.taxisLoading && dataState.countries_CodesLoading
   const dataError = !!dataState.hotelsError || !!dataState.destinationsError || !!dataState.flightsError || !!dataState.trainsError || !!dataState.busesError || !!dataState.taxisError || !!dataState.countries_CodesError
 
-  console.log({ dataLoading, dataError })
-
   const fetchData = async (action, loadingType, errorType) => {
-    dispatch({ type: errorType, payload: null })
     dispatch({ type: loadingType, payload: true })
+    dispatch({ type: errorType, payload: null })
     try {
       await reduxDispatch(action())
     }
     catch (error) {
       dispatch({ type: errorType, payload: error.message })
-      dispatch({ type: loadingType, payload: false })
     }
+    dispatch({ type: loadingType, payload: false })
   }
 
   const loadData = () => {
-    fetchData(fetchHotels, Actions.HOTELS_LOADING, Actions.HOTELS_ERROR)
-    fetchData(fetchDestinations, Actions.DESTINATIONS_LOADING, Actions.DESTINATIONS_ERROR)
-    fetchData(fetchFlights, Actions.FLIGHTS_LOADING, Actions.FLIGHTS_ERROR)
-    fetchData(fetchTrains, Actions.TRAINS_LOADING, Actions.TRAINS_ERROR)
-    fetchData(fetchBuses, Actions.BUSES_LOADING, Actions.BUSES_ERROR)
-    fetchData(fetchTaxis, Actions.TAXIS_LOADING, Actions.TAXIS_ERROR)
-    fetchData(fetchCountriesAndCallingCodes, Actions.COUNTRIES_CODES_LOADING, Actions.COUNTRIES_CODES_ERROR)
+    try {
+      fetchData(fetchHotels, Actions.HOTELS_LOADING, Actions.HOTELS_ERROR)
+      fetchData(fetchDestinations, Actions.DESTINATIONS_LOADING, Actions.DESTINATIONS_ERROR)
+      fetchData(fetchFlights, Actions.FLIGHTS_LOADING, Actions.FLIGHTS_ERROR)
+      fetchData(fetchTrains, Actions.TRAINS_LOADING, Actions.TRAINS_ERROR)
+      fetchData(fetchBuses, Actions.BUSES_LOADING, Actions.BUSES_ERROR)
+      fetchData(fetchTaxis, Actions.TAXIS_LOADING, Actions.TAXIS_ERROR)
+      fetchData(fetchCountriesAndCallingCodes, Actions.COUNTRIES_CODES_LOADING, Actions.COUNTRIES_CODES_ERROR)
+    }
+    catch (error) {
+      Alert.alert('Something Went Wrong!!', error.message, [{ text: 'Okay' }])
+    }
   }
 
   useEffect(() => {
     loadData()
-  }, [reduxDispatch])
-
-  useEffect(() => {
     if (!dataLoading && !dataError) setTimeout(() => { navigation.navigate('HomePage') }, 0)
-  }, [dataLoading, dataError])
+  }, [reduxDispatch, dataLoading, dataError])
+
 
   return (
     <View
